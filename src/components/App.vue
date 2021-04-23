227c8ed7931f19ec1906aa652361511039f3f168
Van Zelleb <76180365+vanzelleb@users.noreply.github.com>
2021-04-23 22:13:39 +0200

    Initial commit

---
+++ src/components/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <Header />
+  <main>
+    <div class="container">
+      BERTRAM And I in going, madam, weep o’er my father’s death anew: but I
+      must attend his majesty’s command, to whom I am now in ward, evermore in
+      subjection. LAFEU You shall find of the king a husband, madam; you, sir, a
+      father: he that so generally is at all times good must of necessity hold
+      his virtue to you; whose worthiness would stir it up where it wanted
+      rather than lack it where there is such abundance. COUNTESS What hope is
+      there of his majesty’s amendment? LAFEU He hath abandoned his physicians,
+      madam; under whose practises he hath persecuted time with hope, and finds
+      no other advantage in the process but only the losing of hope by time.
+      COUNTESS This young gentlewoman had a father,–O, that ‘had’! how sad a
+      passage ’tis!–whose skill was almost as great as his honesty; had it
+      stretched so far, would have made nature immortal, and death should have
+      play for lack of work. Would, for the king’s sake, he were living! I think
+      it would be the death of the king’s disease. LAFEU How called you the man
+      you speak of, madam? COUNTESS He was famous, sir, in his profession, and
+      it was his great right to be so: Gerard de Narbon. LAFEU He was excellent
+      indeed, madam: the king very lately spoke of him admiringly and
+      mourningly: he was skilful enough to have lived still, if knowledge could
+      be set up against mortality.
+    </div>
+  </main>
+  <footer></footer>
+</template>
+
+
+<script>
+import Header from "./Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+main {
+  padding: 0 12px;
+  background-color: white;
+}
+
+footer {
+  padding: 20px 8px;
+  font-size: 0.8rem;
+  text-align: center;
+}
+</style>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <header :class="{ scroll: scrolled }">
+    <div class="container">
+      <h2><span>App name</span></h2>
+    </div>
+  </header>
+</template>
+
+<script>
+import { onMounted, ref } from "vue";
+
+export default {
+  setup() {
+    let scrolled = ref(false);
+
+    const handleScroll = (e) => {
+      scrolled.value = e.target.documentElement.scrollTop > 0;
+    };
+
+    onMounted(() => {
+      window.addEventListener("scroll", handleScroll);
+    });
+
+    return {
+      scrolled,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to component only -->
+<style scoped>
+header {
+  padding: 12px 8px;
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+}
+
+span {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  margin: 0 0;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  max-width: 80ch;
+  margin: 0 auto;
+}
+
+.scroll {
+  -webkit-box-shadow: 0 3px 5px rgba(57, 63, 72, 0.3);
+  -moz-box-shadow: 0 3px 5px rgba(57, 63, 72, 0.3);
+  box-shadow: 0 3px 5px rgba(57, 63, 72, 0.3);
+}
+</style>
